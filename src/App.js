a50e60437fbd327eb24a62a1b7e08bb98284e33c
Carlos Hermida Gómez <yba89200@educastur.es>
2025-02-07 19:33:14 +0100

    cambios en json

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
-import { useEffect, useState } from 'react';
-import Navegacion from './Navegacion';
-import CardClases from './CardClases';
-import CardMaquinas from './CardMaquinas';
+import "./App.css";
+import { useEffect, useState } from "react";
+import Navegacion from "./Navegacion";
+import CardClases from "./CardClases";
+import CardMaquinas from "./CardMaquinas";
 
 function App() {
-
   const [maquinas, setMaquinas] = useState([]);
   const [clases, setClases] = useState([]);
 
@@ -18,15 +17,27 @@ function App() {
     };
     fetchServicios();
   }, []);
-  
+
   return (
     <div style={{ backgroundColor: "#000", minHeight: "100vh", color: "#fff" }}>
       <header className="text-center py-4">
         <Navegacion></Navegacion>
       </header>
+      {clases.length > 0 ? (
         <CardClases clases={clases}></CardClases>
-        <CardMaquinas maquinas={maquinas}></CardMaquinas>
-       
+      ) : (
+        <p className="text-center mt-4">
+          No hay clases disponibles en este momento.
+        </p>
+      )}
+
+      {maquinas.length > 0 ? (
+        <CardMaquinas maquinas={maquinas} />
+      ) : (
+        <p className="text-center mt-4">
+          No hay máquinas disponibles en este momento.
+        </p>
+      )}
     </div>
   );
 }
