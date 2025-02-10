51bb584b24b55e00c47e0875c91ab1ea2fe9431f
Carlos Hermida Gómez <yba89200@educastur.es>
2025-02-10 20:34:32 +0100

    cambios en navbar

--- src/Components/Navegacion.js
+++ src/Components/Navegacion.js
@@ -1,4 +1,4 @@
-import { Nav, Navbar, NavDropdown, Container } from "react-bootstrap";
+import { Nav, Navbar, NavDropdown, Container, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 function Navegacion() {
@@ -6,17 +6,39 @@ function Navegacion() {
     <Navbar bg="dark" expand="lg" variant="dark">
       <Container>
         <Navbar.Brand as={Link} to="/">
-          <img alt="Logo gimnasio" src="/logoGym.png" width={40} height={40} style={{ marginRight: "4%" }} />
-          Ilon Mots GYM</Navbar.Brand>
+          <img
+            alt="Logo gimnasio"
+            src="/logoGym.png"
+            width={40}
+            height={40}
+            style={{ marginRight: "4%" }}
+          />
+          Ilon Mots GYM
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <NavDropdown title="Servicios" id="basic-nav-dropdown" menuVariant="dark">
-              <NavDropdown.Item as={Link} to="/servicios/maquinas">Máquinas</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/servicios/clases">Clases</NavDropdown.Item>
+            <NavDropdown
+              title="Servicios"
+              id="basic-nav-dropdown"
+              menuVariant="dark"
+            >
+              <NavDropdown.Item as={Link} to="/servicios/maquinas">
+                Máquinas
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                as={Link}
+                to="/servicios/clases"
+              ></NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link as={Link} to="/contacto">Contacto</Nav.Link>
-            <Nav.Link as={Link} to="/cliente">Área Cliente</Nav.Link>
+            <Nav.Link as={Link} to="/contacto">
+              Contacto
+            </Nav.Link>
+          </Nav>
+          <Nav className="ms-auto">
+            <Nav.Link as={Link} to="/cliente">
+              <Button variant="warning">Área Cliente</Button>
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
