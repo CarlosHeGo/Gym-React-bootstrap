ea1294565a47e145f927a1eb383f9c8f4be4f1fe
iara.zas <123583066+iarazuska@users.noreply.github.com>
2025-02-14 19:51:36 +0100

    combios en el contacto

--- src/Components/Contacto.js
+++ src/Components/Contacto.js
@@ -1,10 +1,50 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Contacto() {
   return (
-    <div className="text-center py-5">
-      <h1>Página de contacto, pendiente de realizar</h1>
-      <p>Poner formulario para contacto.</p>
+    <div className="d-flex justify-content-center align-items-center vh-100" style={{
+      backgroundImage: "url('/fondo_contacto.jpg')",
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      backgroundRepeat: "no-repeat"
+    }}>
+      <div className="card shadow p-4 w-50 "style={{ background: "rgba(255, 255, 255,0.5)", borderRadius: "10px" }}>
+        <div className="card-body" >
+          <h1 className="text-center mb-4">Contacto</h1>
+          <form>
+            <div className="mb-3">
+              <label className="form-label">Nombre</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Tu nombre"
+              />
+            </div>
+
+            <div className="mb-3">
+              <label className="form-label">Email</label>
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Tu email"
+              />
+            </div>
+
+            <div className="mb-3">
+              <label className="form-label">Mensaje</label>
+              <textarea
+                className="form-control"
+                placeholder="Escribe tu mensaje..."
+              ></textarea>
+            </div>
+
+            <button type="button" className="btn btn-warning w-100">
+              Enviar
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
